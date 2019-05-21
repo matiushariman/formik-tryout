7f3ad5a92f49e0e7fa14d41456b3222ae695e1d0
matiushariman <matius.hariman@gmail.com>
2019-05-21 23:05:42 +0800

    initialize LoginForm component

---
+++ src/LoginForm/index.js
@@ -0,0 +1,12 @@
+/**
+ * LoginForm
+ */
+
+import React from 'react';
+import './styles.css';
+
+function LoginForm() {
+  return <div>This is LoginForm component</div>;
+}
+
+export default LoginForm;
