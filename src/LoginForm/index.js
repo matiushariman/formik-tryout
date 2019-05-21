3d90b61e2f5a50fa101590baeed55d7519761416
matiushariman <matius.hariman@gmail.com>
2019-05-21 23:37:30 +0800

    add basic formik LoginForm

--- src/LoginForm/index.js
+++ src/LoginForm/index.js
@@ -3,10 +3,33 @@
  */
 
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
 import './styles.css';
 
 function LoginForm() {
-  return <div>This is LoginForm component</div>;
+  return (
+    <Formik initialValues={{ email: '', password: '' }}>
+      {props => (
+        <Form>
+          <div className="LoginForm--Container">
+            <div>
+              <div>
+                <Field name="email" type="email" />
+              </div>
+              <div>
+                <Field name="password" type="password" />
+              </div>
+            </div>
+            <div>
+              <pre>
+                <strong>props</strong> = {JSON.stringify(props, null, 2)}
+              </pre>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
 export default LoginForm;
